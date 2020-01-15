68a63c59d9969adb46b21ffa85b65eed96053db0
Austin Mayes <austin@avicus.net>
2020-01-14 22:49:18 -0600

    Remove/rename some ruby stuff

--- Ruby/helpers/layouts_helper.rb
+++
@@ -1,7 +0,0 @@
-module LayoutsHelper
-  # Render a view inside of the specified layout file.
-  def inside_layout(parent_layout)
-    view_flow.set :layout, capture { yield }
-    render template: "layouts/#{parent_layout}"
-  end
-end
--- Ruby/initializers/fix_impressions.rb
+++
@@ -1,6 +0,0 @@
-ImpressionistController::InstanceMethods.class_eval do
-  def session_hash
-    session[:init] = true
-    request.session.id
-  end
-end
